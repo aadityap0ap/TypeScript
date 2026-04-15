a51e8d89b6851a391da7776b3997ccf4956680c2
aadityap0ap <adityabharti450ap@gmail.com>
2026-04-15 18:18:52 +0530

    Done Some HandOn on TS Syntax and Features

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,39 @@
 let x : number = 1;
 console.log(x);
+
+function greet(firstName : string){
+    console.log("Hello" + firstName);
+}
+
+greet("Aditya");
+
+function sum(a:number,b:number){
+    console.log(a+b);
+}
+
+sum(4,50);
+
+function sum1(a:number,b:number){
+    return a + b;
+}
+
+let ans = sum1(4,6);
+console.log(ans);
+
+function isLegal(a:number){
+    if(a >= 18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(isLegal(56));
+
+function startTimer(fn:()=>void){
+    setTimeout(fn,2000);
+}
+startTimer(function(){
+    console.log("Timer Started After 2 sec");
+})
